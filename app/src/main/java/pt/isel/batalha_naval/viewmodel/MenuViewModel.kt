8e32272690d7c6bff00ed6082bbdfdc1396ede53
Vinicius Silva Cordeiro <vinyscordeiro@gmail.com>
2023-01-18 10:18:36 +0000

    Menu View Model.

--- app/src/main/java/pt/isel/batalha_naval/viewmodel/MenuViewModel.kt
+++ app/src/main/java/pt/isel/batalha_naval/viewmodel/MenuViewModel.kt
@@ -5,9 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.Job
 import pt.isel.batalha_naval.domain.Lobby
+import pt.isel.batalha_naval.domain.PlayerInfo
+import pt.isel.batalha_naval.domain.UserInfo
 import pt.isel.batalha_naval.models.LobbyModel
 import pt.isel.batalha_naval.repositories.UserInfoRepository
-import pt.isel.batalha_naval.services.LobbyFirebase
 
 class MenuViewModel (
         private val lobbyService: Lobby,
@@ -35,11 +36,13 @@ class MenuViewModel (
                }
         }
 
-        fun joinLobby(lobby: Lobby) {
+        fun joinLobby(lobbyModel: LobbyModel) : LobbyModel {
+
                 _waitingJob = safeViewModelScopeLaunch {
                         //currentLobby = lobby.enter()
                         //gameId = remoteGameService.join(lobby, userRepository.getUserData()!!.userName)
                 }
+                return LobbyModel(playerInfo = PlayerInfo(UserInfo("123")),123L)
         }
 
         fun leaveLobby() {
